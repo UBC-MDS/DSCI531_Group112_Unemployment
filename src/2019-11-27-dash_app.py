60c73c37974794d797e80cdac0500ff31b9d0d58
marvinmin <55464330+marvinmin@users.noreply.github.com>
2019-11-27 19:33:52 -0800

    added code for the first tab

---
+++ src/2019-11-27-dash_app.py
@@ -0,0 +1,201 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+import altair as alt
+import vega_datasets
+import pandas as pd
+
+app = dash.Dash(__name__, assets_folder='assets')
+app.config['suppress_callback_exceptions'] = True
+
+server = app.server
+app.title = 'Group112 Dash app: Unemployment'
+
+def make_plot(start_year = 2000, end_year = 2001, stat = 'rate'):
+    # Don't forget to include imports
+    def mds_special():
+        font = "Arial"
+        axisColor = "#000000"
+        gridColor = "#DEDDDD"
+        return {
+            "config": {
+                "title": {
+                    "fontSize": 24,
+                    "font": font,
+                    "anchor": "start", # equivalent of left-aligned.
+                    "fontColor": "#000000"
+                },
+                'view': {
+                    "height": 300, 
+                    "width": 400
+                },
+                "axisX": {
+                    "domain": True,
+                    #"domainColor": axisColor,
+                    "gridColor": gridColor,
+                    "domainWidth": 1,
+                    "grid": False,
+                    "labelFont": font,
+                    "labelFontSize": 12,
+                    "labelAngle": 0, 
+                    "tickColor": axisColor,
+                    "tickSize": 5, # default, including it just to show you can change it
+                    "titleFont": font,
+                    "titleFontSize": 16,
+                    "titlePadding": 10, # guessing, not specified in styleguide
+                    "title": "X Axis Title (units)", 
+                },
+                "axisY": {
+                    "domain": False,
+                    "grid": True,
+                    "gridColor": gridColor,
+                    "gridWidth": 1,
+                    "labelFont": font,
+                    "labelFontSize": 14,
+                    "labelAngle": 0, 
+                    #"ticks": False, # even if you don't have a "domain" you need to turn these off.
+                    "titleFont": font,
+                    "titleFontSize": 16,
+                    "titlePadding": 10, # guessing, not specified in styleguide
+                    "title": "Y Axis Title (units)", 
+                    # titles are by default vertical left of axis so we need to hack this 
+                    #"titleAngle": 0, # horizontal
+                    #"titleY": -10, # move it up
+                    #"titleX": 18, # move it to the right so it aligns with the labels 
+                },
+            }
+                }
+
+    # register the custom theme under a chosen name
+    alt.themes.register('mds_special', mds_special)
+
+    # enable the newly registered theme
+    alt.themes.enable('mds_special')
+    #alt.themes.enable('none') # to return to default
+    df_raw = pd.read_csv('../data/unemply_df_year.csv', index_col=0)
+    df = df_raw.drop(columns = ['count', 'rate'])
+    df = df_raw.pivot(index = 'series', columns = 'year', values = 'total').reset_index()
+
+    new_df = pd.DataFrame(df["series"])
+    if stat == "rate":
+        new_df["growth_{0}_{1}".format(start_year, end_year)] = (df[end_year] - df[start_year]) / df[start_year]
+    if stat == "count":
+        new_df["difference_{0}_{1}".format(start_year, end_year)] = df[end_year] - df[start_year]
+        
+    # Create a plot
+
+    chart = alt.Chart(new_df).mark_bar(size=4).encode(
+                alt.X(new_df.columns[1], type = 'quantitative', title = stat),
+                alt.Y("series:O", title=""),
+                tooltip = [new_df.columns[1]]
+            ).properties(title='Job Growth Across Industries',
+                        width=500, height=350).interactive()
+
+    return chart
+
+app.layout = html.Div([
+
+    html.Div(
+        className="app-header",
+        children=[
+            html.Div('Our project name', className="app-header--title")
+        ]
+    ),    
+
+    ### Add Tabs to the top of the page
+    dcc.Tabs(id='tabs', value='tab1', children=[
+        dcc.Tab(label='Job Growth Across Industries', value='tab-1'),
+        dcc.Tab(label='Unemployment Over years', value='tab-2'),
+        dcc.Tab(label='Seasonal Unemployment', value='tab-3'), 
+    ]),    
+
+    html.Iframe(
+        sandbox='allow-scripts',
+        id='plot',
+        height='500',
+        width='1000',
+        style={'border-width': '0'},
+        ################ The magic happens here
+        srcDoc=make_plot().to_html()
+        ################ The magic happens here
+        ),
+        
+        # Just to add some space
+        html.Iframe(height='50', width='10',style={'border-width': '0'}),
+
+        html.H3('Start Year'),
+
+        dcc.Dropdown(
+        id='dd-start_year',
+        options=[
+            {'label': 2000, 'value': 2000},
+            {'label': 2001, 'value': 2001},
+            {'label': 2002, 'value': 2002},
+            {'label': 2003, 'value': 2003},
+            {'label': 2004, 'value': 2004},
+            {'label': 2005, 'value': 2005},
+            {'label': 2006, 'value': 2006},
+            {'label': 2007, 'value': 2007},
+            {'label': 2008, 'value': 2008},
+            {'label': 2009, 'value': 2009}
+        ],
+        value=2000,
+        style=dict(width='45%',
+              verticalAlign="middle"
+              )
+        ),
+
+        html.H3('End Year'),
+
+        dcc.Dropdown(
+        id='dd-end_year',
+        options=[
+            {'label': 2001, 'value': 2001},
+            {'label': 2002, 'value': 2002},
+            {'label': 2003, 'value': 2003},
+            {'label': 2004, 'value': 2004},
+            {'label': 2005, 'value': 2005},
+            {'label': 2006, 'value': 2006},
+            {'label': 2007, 'value': 2007},
+            {'label': 2008, 'value': 2008},
+            {'label': 2009, 'value': 2009},
+            {'label': 2010, 'value': 2010}
+        ],
+        value=2001,
+        style=dict(width='45%',
+              verticalAlign="middle"
+              )
+        ),
+
+        html.H3('Value'),
+
+        dcc.Dropdown(
+        id='dd-value',
+        options=[
+            {'label': 'Rate', 'value': 'rate'},
+            {'label': 'Count', 'value': 'count'}
+        ],
+        value='rate',
+        style=dict(width='45%',
+              verticalAlign="middle"
+              )
+        ),
+        # Just to add some space
+        html.Iframe(height='200', width='10',style={'border-width': '0'})
+])
+
+@app.callback(
+    dash.dependencies.Output('plot', 'srcDoc'),
+    [dash.dependencies.Input('dd-start_year', 'value'),
+     dash.dependencies.Input('dd-end_year', 'value'),
+     dash.dependencies.Input('dd-value', 'value'),])
+def update_plot(start_year, end_year, value):
+
+    updated_plot = make_plot(start_year, end_year, value).to_html()
+
+    return updated_plot
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
